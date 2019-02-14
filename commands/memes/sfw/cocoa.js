6dcb65421845d6bc92f358d762aa66e9486fdd4b
Gabriel Morris <gabemorris@outlook.com>
2019-02-14 14:30:20 -0800

    PROTECTOR OF THE CHOCOLATE BARS

--- commands/memes/sfw/cocoa.js
+++ commands/memes/sfw/cocoa.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
   message.channel.send('https://i.imgur.com/MLmGTll.jpg');
   message.channel
     .send(
-      'Hail <@278301453620084736>, right hand of the revolution\nThe lightkeeper\nBotmaster\nDefender of the Northern Realms'
+      'Hail <@278301453620084736>, right hand of the revolution\nThe lightkeeper\nBotmaster\nDefender of the Northern Realms\nProtector of the chocolate bars!'
     )
     .catch(console.error);
 };
