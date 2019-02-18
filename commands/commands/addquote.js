9e06d9086cdcebf7a2a4a4059049331cbbb5d6ab
Gabriel Morris <gabemorris@outlook.com>
2019-02-18 12:22:28 -0800

    Temp quick fix for quote exploit until I can fix later

--- commands/commands/addquote.js
+++ commands/commands/addquote.js
@@ -1,24 +1,29 @@
 // add quote
 exports.run = (client, message, args) => {
-  const Quote = require('../../include/quote.js');
+  if (
+    message.guild.id === '523227599103590421' ||
+    message.guild.id === '481218726868418570'
+  ) {
+    const Quote = require('../../include/quote.js');
 
-  // murty this is a quote
-  let str = message.content
-    .replace('--addquote ', '', -1)
-    .replace(/-/g, '', -1);
-  // [murty, this, is, a, quote]
-  let strArr = str.split(' ');
-  let author = strArr[0];
-  let quoteToAddArr = strArr.splice(1, strArr.length);
-  let quote = quoteToAddArr.join(' ');
+    // murty this is a quote
+    let str = message.content
+      .replace('--addquote ', '', -1)
+      .replace(/-/g, '', -1);
+    // [murty, this, is, a, quote]
+    let strArr = str.split(' ');
+    let author = strArr[0];
+    let quoteToAddArr = strArr.splice(1, strArr.length);
+    let quote = quoteToAddArr.join(' ');
 
-  if (message.content.length >= 9) {
-    console.log(`Adding quote - Author: ${author} Quote: ${quote}`);
+    if (message.content.length >= 9) {
+      console.log(`Adding quote - Author: ${author} Quote: ${quote}`);
 
-    Quote.insertQuote(message, quote, author);
-  } else {
-    message.channel.send(
-      '**Error**: No quote provided - Syntax: --addquote <author> <quote>'
-    );
+      Quote.insertQuote(message, quote, author);
+    } else {
+      message.channel.send(
+        '**Error**: No quote provided - Syntax: --addquote <author> <quote>'
+      );
+    }
   }
 };
