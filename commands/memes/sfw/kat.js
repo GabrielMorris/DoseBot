92e519feb3a8aed69e06d22c362e97fc98e2ee9e
Gabriel Morris <gabemorris@outlook.com>
2019-02-20 11:16:12 -0800

    update kat appreciation with image

--- commands/memes/sfw/kat.js
+++ commands/memes/sfw/kat.js
@@ -2,7 +2,7 @@
 exports.run = (client, message, args) => {
   message.channel
     .send(
-      'ALL HAIL KAT, <@371151824331210755>\nKAT APPRECIATION DAY\nKAT APPRECIATION DAY\nKAT APPRECIATION DAY\nKAT APPRECIATION DAY'
+      'https://i.imgur.com/yTUeJcT.jpg\n\nALL HAIL KAT, <@371151824331210755>\nKAT APPRECIATION DAY\nKAT APPRECIATION DAY\nKAT APPRECIATION DAY\nKAT APPRECIATION DAY'
     )
     .catch(console.error);
 };
